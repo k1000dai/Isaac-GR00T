3eda701bb2452f9e990a8dc6f41bf0ff78f11c48
k1000dai <chiyodakku1000@gmail.com>
2026-06-16 11:21:23 +0900

    use 12 as openarm_bimanual

--- gr00t/model/gr00t_n1d7/processing_gr00t_n1d7.py
+++ gr00t/model/gr00t_n1d7/processing_gr00t_n1d7.py
@@ -75,7 +75,7 @@ EMBODIMENT_TAG_TO_PROJECTOR_INDEX = {
     "new_embodiment": 10,
     "robocasa_panda_omron": 10,
     "robocasa_gr1_tabletop": 10,
-    "openarm_bimanual": 10,
+    "openarm_bimanual": 12,
 }
 
 
